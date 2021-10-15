fafa5ebb91fadaea2b79c08b38a26753c214bfc5
Jankiparmar311 <91945706+Jankiparmar311@users.noreply.github.com>
2021-10-15 18:48:04 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,84 @@
+const questions = [
+{
+  q1 : "Which type of JavaScript language is ...",
+  a : "A. Object-Oriented",
+  b : "B. Object-Based",
+  c : "C. Assembly-language",
+  d : "D. High-level",
+  ans : "B. Object-Based"
+},
+{
+  q2 : "Which one of the following also known as Conditional Expression:",
+  a : "A. Alternative to if-else",
+  b : "B. Switch statement",
+  c : "C. If-then-else statement",
+  d : "D. immediate if",
+  ans : "D. immediate if"
+},
+{
+  q3 : "In JavaScript, what is a block of statement?",
+  a : "A. Conditional block",
+  b : "B. block that combines a number of statements into a single compound statement",
+  c : "C. both conditional block and a single statement",
+  d : "D. block that contains a single statement",
+  ans : "B. block that combines a number of statements into a single compound statement"
+},
+{
+  q4 : "When interpreter encounters an empty statements, what it will do:",
+  a : "A. Shows a warning",
+  b : "B. Prompts to complete the statement",
+  c : "C. Throws an error",
+  d : "D. Ignores the statements",
+  ans : "D. Ignores the statements"
+},
+{
+  q5 : "Which one of the following is the correct way for calling the JavaScript code?",
+  a : "A. Preprocessor",
+  b : "B. Triggering Event",
+  c : "C. RMI",
+  d : "D. Function/Method",
+  ans : "D. Function/Method"
+},
+{
+  q6 : "In the JavaScript, which one of the following is not considered as an error:",
+  a : "A. Syntax error",
+  b : "B. Missing of semicolons",
+  c : "C. Division by zero",
+  d : "D. Missing of Bracket",
+  ans : "C. Division by zero"
+},
+{
+  q7 : "Which of the following one is the property of the primary expression:",
+  a : "A. Contains only keywords",
+  b : "B. basic expressions containing all necessary functions",
+  c : "C. Assembly-languagecontains variable references alone",
+  d : "D. stand-alone expressions",
+  ans : "D. stand-alone expressions"
+},
+{
+  q8 : "Which one of the following operator is used to check weather a specific property exists or not:",
+  a : "A. Exists",
+  b : "B. exist",
+  c : "C. within",
+  d : "D. in",
+  ans : "D. in"
+},
+{
+  q9 : "Which one of the following is known as the Equality operator, which is used to check whether the two values are equal or not:",
+  a : "A. =",
+  b : "B. ===",
+  c : "C. ==",
+  d : "D. &&",
+  ans : "C. =="
+},
+{
+  q10 : "In a case, where the value of the operator is NULL , the typeof returned by the unary operator is___.",
+  a : "A. undefined",
+  b : "B. string",
+  c : "C. boolean",
+  d : "D. object",
+  ans : "D. object"
+}
+];
+
+module.exports = questions;
